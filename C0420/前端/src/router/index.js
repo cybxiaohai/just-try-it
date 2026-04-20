e1a3942ff710644ddcd0abde85d3c5808b762399
cybxiaohai <cybxiaohai@qq.com>
2026-04-20 11:49:45 +0000

    fix: router import/createWebHashHistory in deployed path

--- C0420/前端/src/router/index.js
+++ C0420/前端/src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import { roleDefaultPath } from '../data/mock'
 import { useAuthStore } from '../stores/auth'
 import Login from '../views/Login.vue'
